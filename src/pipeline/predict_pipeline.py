c3d275cd322df5973bb9f3f64064ecf548262b05
prashantshukla01 <prashantshukla9812@gmail.com>
2025-10-12 00:14:02 +0530

    bugs fixed

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -2,19 +2,19 @@ import sys
 import pandas as pd
 from src.exception import CustomException
 from src.logger import logging
-from src.utils import load_objects
+from src.utils import load_object
 
 class PredictPipeline:
     def __init__(self):
         pass
     
-    def predict(self , feattures):
+    def predict(self, features):
         try:
             model_path = 'artifacts/model.pkl'
             preprocessor_path = 'artifacts/preprocessor.pkl'
-            model = load_objects(file_path=model_path)
-            preprocessor = load_objects(file_path=preprocessor_path)
-            data_scaled = preprocessor.transform(feattures)
+            model = load_object(file_path=model_path)
+            preprocessor = load_object(file_path=preprocessor_path)
+            data_scaled = preprocessor.transform(features)
             preds = model.predict(data_scaled)
             return preds
         except Exception as e:
